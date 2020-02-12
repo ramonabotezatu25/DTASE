004f53c25b47ed6670ab176d50d77154583ec456
Ramona Botezatu <botezatur@playtika.com>
2020-02-12 16:05:57 +0200

    Adding fabric UI

--- dtase/src/components/loginPageComponent/login-form-component.js
+++ dtase/src/components/loginPageComponent/login-form-component.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import "./loginPageComponent.css";
 
+
 const loginForm = props => {
+    
+    
+
+    
     return (
         <div className="login-form-container">
             <div className="login-form">
             <h2>Login</h2>
-            <form>
-                
+            <form name="form">
+
             </form>
             </div>
         </div>
